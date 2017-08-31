6d3813d7221c750b6b8ef162282ab8fad5746f75
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-08-31 07:50:49 +0200

    Required python-mirobo version (>=0.1.3) relaxed.

--- custom_components/light/xiaomi_philipslight.py
+++ custom_components/light/xiaomi_philipslight.py
@@ -28,7 +28,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
-REQUIREMENTS = ['python-mirobo>=0.1.4']
+REQUIREMENTS = ['python-mirobo>=0.1.3']
 
 # The light does not accept cct values < 1
 CCT_MIN = 1
