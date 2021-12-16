fce362ffe228dc12bbe2d60ac278c91bfd3425f0
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2021-12-16 17:02:48 +0200

    Rename deprecated device_state_attributes to extra_state_attributes

--- custom_components/xiaomi_miio_philipslight/light.py
+++ custom_components/xiaomi_miio_philipslight/light.py
@@ -272,7 +272,7 @@ class XiaomiPhilipsAbstractLight(LightEntity):
         return self._available
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes of the device."""
         return self._state_attrs
 
